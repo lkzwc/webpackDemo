aa69d89417bd0cd187612181aa3157cc1e66705f
lkzwc <lkzwc@outlook.com>
2021-12-05 23:01:41 +0800

    start

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { resolve } = require("path");
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+    login: "./src/a.js",
+  },
+  output: {
+    path: resolve(__dirname, "./build"),
+    filename: "[name].js", //用占位符替代
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"], //从后向前
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/public/index.html", //指定模板
+      filename: "index.html",
+      chunks: ["index"], //区分html中导入的js墨客
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/public/login.html",
+      filename: "login.html",
+      chunks: ["login"],
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  mode: "production",
+};
